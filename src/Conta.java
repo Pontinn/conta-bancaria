338197f31bef249b6520562da03e81454b070fe3
Pontinn <leo.pontin2@gmail.com>
2025-05-23 13:25:10 -0300

    Creating initial methods and variables

--- src/Conta.java
+++ src/Conta.java
@@ -1,2 +1,5 @@
 public interface Conta {
+    void Deposit();
+    void Withdraw();
+    void TotalBalance();
 }
--- src/ContaBancaria.java
+++ src/ContaBancaria.java
@@ -1,2 +1,19 @@
-public abstract class ContaBancaria {
+public abstract class ContaBancaria implements Conta{
+
+    double balance = 0;
+
+    @Override
+    public void Deposit() {
+
+    }
+
+    @Override
+    public void Withdraw() {
+
+    }
+
+    @Override
+    public void TotalBalance() {
+
+    }
 }
